7f3a3c394048e97327e5bc72717f19b1fc142b69
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-02 09:43:38 -0400

    Profile page working

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :rating, :user_id, :vinyl_id
+  attributes :id, :title, :content, :rating, :user_id, :vinyl_id, :reviewed_vinyl
 
   belongs_to :vinyl
   belongs_to :user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :wants
   resources :reviews
   resources :vinyls, only: [:index]
-  resources :users, only: [:create, :index]
+  resources :users, only: [:create, :index, :show]
   post "/login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
